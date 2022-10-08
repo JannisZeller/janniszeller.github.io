6a7aed770845c77eeab2667121197257dc2cab7d
jzelle <jannis.zeller@arcor.de>
2022-10-08 17:57:32 +0200

    Minor changes

--- lectures/probml/bgmobile.js
+++ lectures/probml/bgmobile.js
@@ -1,8 +1,34 @@
-var bg = $("#background");
+var bg = $("#bg1");
 
 function resizeBackground() {
-    bg.height($(window).height() + 60);
+    bg.height($(window).height());
+    console.log("TestBgResize");
 }
 
 $(window).resize(resizeBackground);
 resizeBackground();
+
+$(window).on('load', function() {
+    console.log("TestLoad")
+    resizeBackground();
+});
+
+$(window).on('resize', function() {
+    console.log("TestResize")
+    resizeBackground();
+});
+
+// var background = document.getElementById('background');
+
+// // Fix background image jump on mobile
+// if ((/Android|iPhone|iPad|iPod|BlackBerry/i).test(navigator.userAgent || navigator.vendor || window.opera)) {
+//   background.style.top = 'auto';
+//   background.style.bottom = 0;
+
+//   window.onresize = sizeBackground;
+//   sizeBackground();
+// }
+
+// function sizeBackground() {
+//   background.style.height = screen.height;
+// }
--- lectures/probml/probml.js
+++ lectures/probml/probml.js
@@ -1,14 +1,24 @@
 var scrolls = document.getElementsByClassName('scroll');
+var mathjax_scale = 1;
+
+MathJax = {
+  chtml: {
+    scale: mathjax_scale,
+  },
+  tex: {
+    inlineMath: [['$', '$']]
+  }
+};
 
 color_scroll_divs = function(scrolls) {
     var k = 0;
     for (let div of scrolls) {
         k++;
         var parent = div.parentElement;
-        if (parent.tagName == "LI") {
+        while (parent.tagName == "LI") {
             parent = parent.parentElement;
         }
-        if (div.scrollWidth > parent.clientWidth) {
+        if (div.scrollWidth  > parent.clientWidth) {
             div.style.overflowX = "auto";
             div.style.overflowY = "hidden";
             div.style.background = "rgba(255,255,255,.05)";
@@ -20,20 +30,27 @@ color_scroll_divs = function(scrolls) {
             div.style.background = "rgba(255,255,255,.00)";
             div.style.marginTop = "0";
         }
+        // Debugging
+        // if (k==1) {
+        //     console.log(div.scrollWidth);
+        //     console.log(parent);
+        //     console.log(parent.clientWidth);
+        // }
     }   
     console.log("Scanning and Converting Formula-Divs.");
-    console.log("Numbers of indented Formulas " + k.toString());
+    console.log("Numbers of Formulas " + k.toString());
 }
 
 window.onload = function() {
     color_scroll_divs(scrolls);
 
-    var background = document.getElementById('background');
-    background.style.opacity = 0.5;
+    var bg2 = document.getElementById('bg2');
+    bg2.style.opacity = 0.5;
 
     var content = document.getElementsByClassName('content')[0];
     content.style.visibility = 'visible';
     content.style.opacity = 0.88;
+
     console.log(content)
 }
 
